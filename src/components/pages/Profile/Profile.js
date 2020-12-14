666ab87ba8022bae3c89096f4169210c5c71af51
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-14 21:11:48 +0100

    first commit

---
+++ src/components/pages/Profile/Profile.js
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap'
+
+const Profile = ({ loggedUser }) => {
+    return (
+        <Container>
+            <h1>¡Bienvenid@, {loggedUser.username}!</h1>
+        </Container>
+    )
+}
+
+export default Profile
